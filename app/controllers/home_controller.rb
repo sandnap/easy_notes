c88542275cb9785c76a6c7697c1f6aa6ad00742e
Kirt Henrie <sandnap@gmail.com>
2024-12-11 16:33:29 -0700

    Finished working with turbo links

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,5 @@
 class HomeController < ApplicationController
   def index
     @categories = Current.user.categories.includes(:notes)
-    respond_to do |format|
-      format.turbo_stream do
-        Rails.logger.debug "Turbo stream request received"
-        render turbo_stream: [
-          turbo_stream.update("main", partial: "home/select_note")
-        ]
-      end
-      Rails.logger.debug "format.html"
-      format.html { render :index }
-    end
   end
 end
